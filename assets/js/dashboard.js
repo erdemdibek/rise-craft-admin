c95f08435f90918d94ab4e460428ed6b14ab7201
erdemdibek <erdemdibek9393@gmail.com>
2026-07-21 13:40:31 +0300

    Delete dashboard.js

--- assets/js/dashboard.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("recipeCount").innerText="0";
-
-document.getElementById("version").innerText="1";
-
-document.getElementById("lastCommit").innerText="-";
-
-document.getElementById("status").innerHTML="Online";
