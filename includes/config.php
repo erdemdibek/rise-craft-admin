1b5e138d1f80449ec0a9b4567b3d5a050ac63fc6
erdemdibek <erdemdibek9393@gmail.com>
2026-07-21 10:52:36 +0300

    Create includes/config.php

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+define('APP_NAME', 'Rise Craft Admin');
+
+define('ADMIN_USERNAME', 'admin');
+define('ADMIN_PASSWORD', '1993000Ee**'); // Sonra hash yapacağız
+
+define('GITHUB_OWNER', '');
+define('GITHUB_REPO', '');
+define('GITHUB_BRANCH', 'main');
+define('GITHUB_TOKEN', '');
