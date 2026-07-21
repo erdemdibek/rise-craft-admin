c46503bd3b27eef93ac22669a34b5295c1acc46d
erdemdibek <erdemdibek9393@gmail.com>
2026-07-21 14:31:32 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,29 +1,14 @@
 const CONFIG = {
 
-    GITHUB: {
+    OWNER: "erdemdibek",
 
-        OWNER: "erdemdibek",
+    REPO: "rise-craft-companion-data",
 
-        REPO: "rise-craft-companion-data",
+    BRANCH: "main",
 
-        BRANCH: "main",
+    TOKEN: "",
 
-        RAW_BASE:
-            "https://raw.githubusercontent.com/erdemdibek/rise-craft-companion-data/main",
-
-        API_BASE:
-            "https://api.github.com/repos/erdemdibek/rise-craft-companion-data"
-
-    },
-
-    PATHS: {
-
-        MANIFEST: "manifest.json",
-
-        PROFESSIONS: "modules/recipes.json",
-
-        RECIPES: "modules/recipes"
-
-    }
+    RAW_BASE:
+        "https://raw.githubusercontent.com/erdemdibek/rise-craft-companion-data/main"
 
 };
