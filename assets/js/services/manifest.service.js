d64372aac087b853305242903cac1f4931baf5ad
erdemdibek <erdemdibek9393@gmail.com>
2026-07-21 14:35:30 +0300

    Update manifest.service.js

--- assets/js/services/manifest.service.js
+++ assets/js/services/manifest.service.js
@@ -1,19 +1,54 @@
 class ManifestService {
 
-    constructor(){
+    constructor() {
 
-        this.github=new GithubService();
+        this.path = "manifest.json";
 
     }
 
-    async load(){
+    async load(forceRefresh = false) {
 
-        return await this.github.getJson(
+        return await githubService.downloadJson(
+            this.path,
+            forceRefresh
+        );
 
-            CONFIG.PATHS.MANIFEST
+    }
+
+    async save(manifest) {
+
+        return await githubService.uploadJson(
+
+            this.path,
+
+            manifest,
+
+            `Update manifest v${manifest.contentVersion}`
 
         );
 
     }
 
+    incrementRecipeVersion(manifest) {
+
+        manifest.contentVersion++;
+
+        manifest.modules.recipes.version++;
+
+        return manifest;
+
+    }
+
+    incrementMaterialVersion(manifest) {
+
+        manifest.contentVersion++;
+
+        manifest.modules.materials.version++;
+
+        return manifest;
+
+    }
+
 }
+
+const manifestService = new ManifestService();
