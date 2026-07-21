21f84a8f4ef493de2ed55f4966e932c9ca5cef00
erdemdibek <erdemdibek9393@gmail.com>
2026-07-21 11:14:35 +0300

    Create sidebar.js

---
+++ assets/js/sidebar.js
@@ -0,0 +1,69 @@
+document.getElementById("sidebar").innerHTML=`
+
+<div class="sidebar">
+
+<h2>Rise Craft</h2>
+
+<a href="dashboard.html">
+
+<i class="bi bi-speedometer2"></i>
+
+Dashboard
+
+</a>
+
+<a href="recipes.html">
+
+<i class="bi bi-box"></i>
+
+Recipes
+
+</a>
+
+<a href="editor.html">
+
+<i class="bi bi-pencil-square"></i>
+
+Editor
+
+</a>
+
+<hr>
+
+<a href="#">
+
+<i class="bi bi-gear"></i>
+
+Settings
+
+</a>
+
+</div>
+
+`;
+
+document.getElementById("navbar").innerHTML=`
+
+<div class="topbar">
+
+<div>
+
+<h4>Dashboard</h4>
+
+</div>
+
+<div>
+
+<button class="btn btn-warning">
+
+<i class="bi bi-arrow-repeat"></i>
+
+Sync
+
+</button>
+
+</div>
+
+</div>
+
+`;
