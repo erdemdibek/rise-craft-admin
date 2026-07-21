9a5193a369c83ec5cec0b4f5ad12d8e162bf153d
erdemdibek <erdemdibek9393@gmail.com>
2026-07-21 14:36:22 +0300

    Update publish.service.js

--- assets/js/services/publish.service.js
+++ assets/js/services/publish.service.js
@@ -4,6 +4,8 @@ class PublishService {
 
         this.changedFiles = new Map();
 
+        this.isPublishing = false;
+
     }
 
     addRecipeFile(profession, recipes) {
@@ -12,7 +14,7 @@ class PublishService {
 
             `modules/recipes/${profession}.json`,
 
-            recipes
+            structuredClone(recipes)
 
         );
 
@@ -24,15 +26,110 @@ class PublishService {
 
     }
 
+    getFiles() {
+
+        return [...this.changedFiles.entries()];
+
+    }
+
     clear() {
 
         this.changedFiles.clear();
 
     }
 
-    getFiles() {
+    async publish() {
 
-        return [...this.changedFiles.entries()];
+        if (this.isPublishing)
+            return;
+
+        if (!this.hasChanges()) {
+
+            alert("Publish edilecek değişiklik yok.");
+
+            return;
+
+        }
+
+        this.isPublishing = true;
+
+        try {
+
+            const manifest = await manifestService.load();
+
+            // Önce recipe dosyalarını yükle
+
+            for (const [path, recipes] of this.changedFiles) {
+
+                await githubService.uploadJson(
+
+                    path,
+
+                    recipes,
+
+                    `Update ${path}`
+
+                );
+
+            }
+
+            // Manifest versiyonunu artır
+
+            manifestService.incrementRecipeVersion(
+
+                manifest
+
+            );
+
+            // Manifest'i yükle
+
+            await manifestService.save(
+
+                manifest
+
+            );
+
+            // Draft temizle
+
+            this.changedFiles.forEach((_, path) => {
+
+                const profession = path
+                    .replace("modules/recipes/", "")
+                    .replace(".json", "");
+
+                state.clearDraft(profession);
+
+            });
+
+            this.clear();
+
+            githubService.clearCache();
+
+            navbar.update();
+
+            alert("Publish başarıyla tamamlandı.");
+
+        }
+
+        catch (e) {
+
+            console.error(e);
+
+            alert(
+
+                "Publish sırasında hata oluştu.\n\n" +
+
+                e.message
+
+            );
+
+        }
+
+        finally {
+
+            this.isPublishing = false;
+
+        }
 
     }
 
