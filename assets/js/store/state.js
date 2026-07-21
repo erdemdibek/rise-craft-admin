b49f3d8ed8cbd0147a20e811a22afdab07a8aa94
erdemdibek <erdemdibek9393@gmail.com>
2026-07-21 14:04:59 +0300

    Create store/state.js

---
+++ assets/js/store/state.js
@@ -0,0 +1,75 @@
+class AppState {
+
+    constructor() {
+
+        this.manifest = null;
+
+        this.professions = {};
+
+        this.currentProfession = null;
+
+        this.recipes = [];
+
+        this.selectedRecipe = null;
+
+        this.drafts = {};
+
+    }
+
+    setManifest(manifest) {
+
+        this.manifest = manifest;
+
+    }
+
+    setProfessions(professions) {
+
+        this.professions = professions;
+
+    }
+
+    setCurrentProfession(profession) {
+
+        this.currentProfession = profession;
+
+    }
+
+    setRecipes(recipes) {
+
+        this.recipes = recipes;
+
+    }
+
+    setSelectedRecipe(recipe) {
+
+        this.selectedRecipe = recipe;
+
+    }
+
+    saveDraft(profession, recipes) {
+
+        this.drafts[profession] = structuredClone(recipes);
+
+    }
+
+    getDraft(profession) {
+
+        return this.drafts[profession] ?? null;
+
+    }
+
+    hasDraft(profession) {
+
+        return profession in this.drafts;
+
+    }
+
+    clearDraft(profession) {
+
+        delete this.drafts[profession];
+
+    }
+
+}
+
+const state = new AppState();
